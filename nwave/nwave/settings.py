e83a3bc66ac61137ef78af04c16945eba05476bc
Eric <ericr7226@gmail.com>
2021-06-14 18:13:52 -0700

    added class to import data from CSV files

--- nwave/nwave/settings.py
+++ nwave/nwave/settings.py
@@ -9,11 +9,14 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+PARQUET_FILES_DIR = BASE_DIR.parent / 'parquet_files'
+CSV_PATH = BASE_DIR.parent / 'challenge_datasets'
+os.makedirs(PARQUET_FILES_DIR, 0o777, exist_ok=True)
 
 
 # Quick-start development settings - unsuitable for production
--- utils.py
+++ utils.py
@@ -4,22 +4,20 @@ import pandas as pd
 from pathlib import Path
 
 
-class ImportCSV(object):
-    def __init__(self):
-        self.base_path = Path(os.path.abspath(os.getcwd()))
-        self.pq_path = self.base_path / 'parquet_files'
-        self.csv_path = self.base_path / 'challenge_datasets'
-        os.makedirs(self.pq_path, 0o777, exist_ok=True)
+class ImportDataFromCSV(object):
+    def __init__(self, csv_path):
+        self.csv_path = csv_path
+        self.df = None
 
     def get_data_from_csv(self):
         frames = []
-        for csv_file in get_csv_files(self.csv_path):
+        for csv_file in yield_csv_file_rows(self.csv_path):
             # print(csv_file)
             df = pd.read_csv(csv_file)
             df.fillna('', inplace=True)
 
-            # # convert timestamp to datetime
-            # df['timestamp'] = pd.to_datetime(df['timestamp'])
+            # convert timestamp to datetime
+            df['timestamp'] = pd.to_datetime(df['timestamp'])
 
             # extract tag column
             df[['empty', 'site', 'asset', 'column']] = df['tag'].str.split(
@@ -30,11 +28,19 @@ class ImportCSV(object):
             del df['empty']
             frames.append(df)
 
-        # return combined dataframes
-        return pd.concat(frames)
+        # combine dataframes, pivot on timestamp/asset and return data
+        self.df = pd.concat(frames)
+        self.df.groupby('asset').cumcount()
+        self.df = self.df.pivot(
+            index=['timestamp', 'asset'],
+            columns='column',
+            values='value'
+        )
+        self.df.reset_index(inplace=True)
+        return self.df
 
 
-def get_csv_files(file_path):
+def yield_csv_file_rows(file_path):
     csv_list = list((
         str(file_path / f) for f in os.listdir(file_path)
     ))
@@ -47,28 +53,13 @@ if __name__ == '__main__':
 
     # set base and parquet out files path
     base_path = Path(os.path.abspath(os.getcwd()))
-    pq_path = base_path / 'parquet_files'
-    os.makedirs(pq_path, 0o777, exist_ok=True)
+    path_to_csv_files = base_path / 'challenge_datasets'
+    # pq_path = base_path / 'parquet_files'
+    # os.makedirs(pq_path, 0o777, exist_ok=True)
 
-    # pull data from csv files into dataframe
-    n_wave_df = get_data_from_csv(base_path)
-
-    # all_csv_data = get_data_from_csv(base_path)
-    # site_name = all_csv_data.site.unique()
-    # n_wave_df = all_csv_data[
-    #     ['timestamp', 'asset', 'column', 'value']
-    # ].copy()
-    print(f'got all data')
-    # pivot on asset
-    # n_wave_df.groupby('tag').cumcount()
-    # assets = n_wave_df.asset.unique()
-    # cols = n_wave_df.column.unique()
-    # print(assets, len(assets))
-    # print(cols, len(cols))
-    # n_wave_df.pivot_table(index='tag', columns='timestamp', values='value')
-    # pd.set_option('display.max_rows', 5, 'display.max_columns', None)
-    # print(n_wave_df)
-    # exit()
+    nwave = ImportDataFromCSV(path_to_csv_files)
+    nw_df = nwave.get_data_from_csv()
+    print('got pivoted data')
 
     et = time.perf_counter()
     print(round(et - bt, 2))
